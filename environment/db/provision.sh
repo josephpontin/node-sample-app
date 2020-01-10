f7a72144c222fc0e58fd28da0db7d9f024d7429b
Joseph Pontin <JPontin@spartaglobal.com>
2020-01-10 12:13:40 +0000

    Provision improved

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -1,14 +1,28 @@
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+
+# install mongodb 3.2.20
+
+# key needed to access repo
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+# gets url of that source and adds to list
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-
+# pulls from source
 sudo apt-get update -y
-sudo apt-get upgrade -y
-
-# sudo apt-get install mongodb-org=3.2.18 -y
+# installs
 sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
 
-sudo rm /etc/mongod.conf
-sudo ln -s /home/ubuntu/environment/db/mongod.conf /etc/mongod.conf
+# update sources list
+sudo apt-get update -y
 
+# upgrade packages
+#sudo apt-get upgrade -y
+
+# remove existing mongod.conf
+sudo rm /etc/mongod.conf
+# link to mongod.conf shared from host - synced connection
+sudo ln -s /home/ubuntu/environment/mongod.conf /etc/mongod.conf
+
+# start mongodb
 sudo systemctl restart mongod
+
+# enable service
 sudo systemctl enable mongod
